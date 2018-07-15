75fed8fe677c02978d2b4d3d40caf5932b063d8f
strangechu <strangechu@gmail.com>
2018-07-16 04:24:01 +0900

    first commit

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+    Camera mainCamera;
+    public GameObject arrow;
+    public float speed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            this.transform.Rotate(0, Input.GetAxis("Mouse X") * speed, 0);
+        }
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            this.transform.Rotate(-Input.GetAxis("Mouse Y") * speed, 0, 0);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            this.transform.position += this.transform.forward * speed;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            this.transform.position -= this.transform.forward * speed;
+        }
+    }
+
+    public void UpdatePosition()
+    {
+        arrow.transform.position = mainCamera.transform.position + mainCamera.transform.forward * 2;
+        arrow.transform.forward = mainCamera.transform.forward;
+    }
+}
---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Boid boid = GetComponent<FlockManager>().getBoid(0);
+        if (boid)
+        {
+            //Camera.main.transform.position = boid.transform.position - Camera.main.transform.forward * 10 + Camera.main.transform.up * 0;
+            //Camera.main.transform.forward = boid.transform.forward;
+        }
+	}
+}
