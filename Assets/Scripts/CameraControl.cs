75cfae80905d00926a4a8b7c8663e09c0dad3a47
strangechu <strangechu@gmail.com>
2018-09-27 23:46:41 +0900

    add top camera view

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public static CameraControl instance = null;
+
+    public Camera mainCamera;
+    public Camera topCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        mainCamera.enabled = true;
+        topCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SwitchCamera()
+    {
+        mainCamera.enabled = !mainCamera.enabled;
+        topCamera.enabled = !topCamera.enabled;
+        Debug.Log("Camera switched!");
+    }
+}
---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+
+    public static UIControl instance = null;
+
+    public Button switchCameraBtn;
+
+    // Use this for initialization
+    void Start () {
+        if (instance == null)
+            instance = this;
+        switchCameraBtn.onClick.AddListener(OnClickSwitchCameraBtn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClickSwitchCameraBtn ()
+    {
+        CameraControl.instance.SwitchCamera();
+    }
+}
