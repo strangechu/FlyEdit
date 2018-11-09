c877aacb96c3554eff5913342ae7bd51d330b056
strangechu <strangechu@gmail.com>
2018-11-09 16:35:10 +0900

    link with BirdOpti

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +10,15 @@ public class UIControl : MonoBehaviour {
     public static UIControl instance = null;
 
     public Button switchCameraBtn;
+    public Button optimizeBtn;
 
     // Use this for initialization
     void Start () {
         if (instance == null)
             instance = this;
         switchCameraBtn.onClick.AddListener(OnClickSwitchCameraBtn);
-	}
+        optimizeBtn.onClick.AddListener(OnClickOptimizeBtn);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,5 +28,29 @@ public class UIControl : MonoBehaviour {
     void OnClickSwitchCameraBtn ()
     {
         CameraControl.instance.SwitchCamera();
+    }
+
+    void OnClickOptimizeBtn()
+    {
+        try
+        {
+            print("Optimization start.");
+            Process myProcess = new Process();
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            myProcess.StartInfo.CreateNoWindow = false;
+            myProcess.StartInfo.UseShellExecute = false;
+            string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "BirdOpti.exe");
+            myProcess.StartInfo.FileName = filePath;
+            myProcess.StartInfo.Arguments = filePath;
+            myProcess.EnableRaisingEvents = true;
+            myProcess.Start();
+            myProcess.WaitForExit();
+            int ExitCode = myProcess.ExitCode;
+        }
+        catch (Exception e)
+        {
+            print(e);
+        }
+        print("Optimization end.");
     }
 }
